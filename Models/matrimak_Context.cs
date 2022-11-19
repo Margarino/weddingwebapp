0c7d93f483b8602776e2bf66ddf463a1a66d5999
Margarino <i.quirozaguilera@gmail.com>
2022-11-18 23:08:02 -0300

    a
    a

--- Models/matrimak_Context.cs
+++ Models/matrimak_Context.cs
@@ -26,7 +26,7 @@ namespace weddingWebapp.Models
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
                 optionsBuilder.UseMySQL("Server=localhost; port=3306;Database=matrimak_;Uid=root;Pwd=1984;");
-            }
+            } 
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
