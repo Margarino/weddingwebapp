aac8def1303564e4f34b65c95791f31b890b1c38
Miguel Espinoza <Miguel.espinoza@alumnos.uv.cl>
2022-11-21 01:19:28 -0300

    Auto stash before merge of "master" and "origin/master"

--- Models/MailModel.cs
+++ Models/MailModel.cs
@@ -6,5 +6,6 @@
         public string Email { get; set; }
         public string Nota { get; set; }
         public string Monto { get; set; }
+        public string NombreRegalo { get; set; }
     }
 }
